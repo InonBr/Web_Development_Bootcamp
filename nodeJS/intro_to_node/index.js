84cee8f6cb06077d7e82f07bb1f3fd0ddb97c26e
Inon Bercu <inonbr@gmail.com>
2020-01-12 14:34:02 +0200

    added one more package

--- nodeJS/intro_to_node/index.js
+++ nodeJS/intro_to_node/index.js
@@ -1,3 +1,8 @@
+// require packages
 const fs = require('fs');
+const superheroes = require('superheroes');
 
-fs.copyFileSync("file.txt", "file2.txt");
+// fs.copyFileSync("file.txt", "file2.txt");
+
+let myHero = superheroes.random()
+console.log(myHero);
